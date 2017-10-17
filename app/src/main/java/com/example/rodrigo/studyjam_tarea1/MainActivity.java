ae5c083ca08e08c06dbab018049cf5d7c45f6f19
Rodrigo Espinoza <espinozarjc@gmail.com>
2017-10-17 03:03:48 -0400

    Commit inicial

---
+++ app/src/main/java/com/example/rodrigo/studyjam_tarea1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.rodrigo.studyjam_tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+    }
+}
